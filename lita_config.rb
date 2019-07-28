b31b75d9954bd1ab42aa544e14c0ea9f7efe2e81
Tad Thorley <phaedryx@gmail.com>
2019-07-27 18:34:50 -0600

    put slack back for now

--- lita_config.rb
+++ lita_config.rb
@@ -7,7 +7,7 @@ Dir['./lita/**/*.rb'].each { |file| require file }
 Lita.configure do |config|
   if ENV['LITA_ENVIRONMENT'] == 'production'
     config.redis[:url]     = ENV['REDIS_URL']
-    # config.robot.adapter   = :slack
+    config.robot.adapter   = :slack
     config.robot.log_level = :info
   else
     config.redis           = { host: '127.0.0.1', port: 6379 }
@@ -21,7 +21,7 @@ Lita.configure do |config|
   config.http.port    = ENV['HTTP_PORT']
 
   # slack config
-  # config.adapters.slack.token      = ENV['SLACK_TOKEN']
-  # config.adapters.slack.link_names = true
-  # config.adapters.slack.parse      = 'full'
+  config.adapters.slack.token      = ENV['SLACK_TOKEN']
+  config.adapters.slack.link_names = true
+  config.adapters.slack.parse      = 'full'
 end
